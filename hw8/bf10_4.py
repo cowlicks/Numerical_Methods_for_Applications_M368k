fe8215bc0267496202b15d630a1ba1d8679f660c
Blake Griffith <blake.a.griffith@gmail.com>
2013-03-27 09:48:27 -0500

    Finished steepest descent section of written assignment.

--- hw8/bf10_4.py
+++ hw8/bf10_4.py
@@ -3,18 +3,21 @@ import numpy
 
 class one:
     def __init__(self):
-        self.x0 = (1., 1.5)
+        self.x0 = numpy.array([[1.],[ 1.5]])
 
     def g(self, x):
-        return (3*x[0]**2 - x[1]**2)**2 + (3*x[0]*x[1]**2 - x[0]**3 - 1)**2
+        return float((3*x[0]**2 - x[1]**2)**2 + (3*x[0]*x[1]**2 - x[0]**3 - 1)**2)
 
-    def gg(self, x):
-        x0 = x[0]
-        x1 = x[1]
+    def dg(self, x):
+        x0 = float(x[0])
+        x1 = float(x[1])
         a   = 2*(3*x0**2 - x1**2)*(6*x0) + 2*(3*x0*x1**2 - x0**3 - 1)*(3*x1**2 - 3*x0**2)
         b   = 2*(3*x0**2 - x1**2)*(-2*x1) + 2*(3*x0*x1**2 - x0**3 - 1)*(6*x1)
-        return a, b
+        return numpy.array([[a], [b]])
 
     def z0(self, x):
-        a, b = self.gg(x)
-        return math.sqrt(a**2 + b**2)
+        y = self.dg(x)
+        return math.sqrt(y[0]**2 + y[1]**2)
+
+    def z(self, x):
+        return self.dg(x)/self.z0(x)
